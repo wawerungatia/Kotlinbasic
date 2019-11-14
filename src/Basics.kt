82665bc524a7151b4ea14ebe8b849a10b039f8ae
wawerungatia <joseph.waweru@riarauniversity.ac.ke>
2019-11-14 14:58:47 +0300

    feat:numbers in kotlin

---
+++ src/Basics.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>){
+    val a:Int=10000
+    val d:Double=100.00
+    val l:Long=100000004
+    val f:Float=100.00f
+    val s:Short=10
+    val b:Byte=1
+    println("Your int Value is"+a);
+    println("Your Double Value is"+d);
+    println("Your long Value is"+l);
+    println("Your Float Value is"+f);
+    println("Your Short Value is"+s);
+    println("Your Byte Value is"+b);
+}
